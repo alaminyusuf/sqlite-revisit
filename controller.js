39eead8640216682e73d93b20f295aea07ee5d7d
alamin__yusuf <alaminyusuf131@gmail.com>
2024-05-28 12:18:53 +0000

    added update and delete to rest API

--- controller.js
+++ controller.js
@@ -1,28 +1,50 @@
-const Student = require('./model')
-
+const Student = require("./model");
 
 exports.getAllStudents = async (req, res) => {
-    try {
-        const students = await Student.findAll()
-        return res.json(students)
-    } catch (e) {
-        console.log(e);
-       return  res.status(500).send('Internal Server Error')
-    }
-}
+	try {
+		const students = await Student.findAll();
+		return res.json(students);
+	} catch (e) {
+		console.log(e);
+		return res.status(500).send("Internal Server Error");
+	}
+};
 
 exports.createStudent = async (req, res) => {
-    try {
-        const{
-            name,
-            dept,
-            level,
-            grade,
-        } = req.body;
-        const student = await Student.create({name, dept, level, grade})
-        return res.json({student})
-    } catch (e) {
-        console.log(e);
-        return res.status(500).send('Internal Server Error')
-    }
-}
+	try {
+		const { name, dept, level, grade } = req.body;
+		const student = await Student.create({ name, dept, level, grade });
+		return res.json({ student });
+	} catch (e) {
+		console.log(e);
+		return res.status(500).send("Internal Server Error");
+	}
+};
+
+exports.updateStudent = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const userFound = await Student.update(req.body, {
+			where: { id: userId },
+		});
+		if (userFound === 0) {
+			return res.status(404).send("Student not Found");
+		}
+		const updatedUser = await Student.findByPk(userId);
+		return res.json({ updatedUser });
+	} catch (e) {
+		console.log(e);
+		return res.status(500).send("Internal Server Error");
+	}
+};
+
+exports.deleteStudent = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		await Student.destroy({ where: { id: userId } });
+		return res.status(200).json({ msg: "Student deleted" });
+	} catch (e) {
+		console.log(e);
+		return res.status(404).send("Could not delete, Student not found");
+	}
+};
--- routes.js
+++ routes.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const {createStudent, getAllStudents} = require('./controller')
-const router = express.Router()
+const express = require("express");
+const {
+	createStudent,
+	getAllStudents,
+	updateStudent,
+	deleteStudent,
+} = require("./controller");
+const router = express.Router();
 
-router.route("/students").get(getAllStudents).post(createStudent)
+router.route("/students").get(getAllStudents).post(createStudent);
+
+router.route("/students/:id").put(updateStudent).delete(deleteStudent);
 
 module.exports = router;
